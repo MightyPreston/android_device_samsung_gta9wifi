68f859f0c876a5e7b404a2295f316ad9cef6a2d7
MightyPreston <StudioTube@knueck.ch>
2026-05-04 08:52:48 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-add_lunch_combo omni_SRPWF07A006-user
-add_lunch_combo omni_SRPWF07A006-userdebug
-add_lunch_combo omni_SRPWF07A006-eng
+add_lunch_combo omni_gta9wifi-user
+add_lunch_combo omni_gta9wifi-userdebug
+add_lunch_combo omni_gta9wifi-eng
 
