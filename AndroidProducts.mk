2a67219b9aa27019c5bbab3abb6bc4fd6cf6de99
MightyPreston <StudioTube@knueck.ch>
2026-05-04 09:14:53 +0200

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,10 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_gta9wifi.mk
+    $(LOCAL_DIR)/twrp_gta9wifi.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_gta9wifi-user \
-    omni_gta9wifi-userdebug \
-    omni_gta9wifi-eng
+    twrp_gta9wifi-user \
+    twrp_gta9wifi-userdebug \
+    twrp_gta9wifi-eng
 
