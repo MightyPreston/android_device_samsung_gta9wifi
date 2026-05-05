1fdb6c042cbbd62d20a95e63529c25710317442b
MightyPreston <StudioTube@knueck.ch>
2026-05-05 12:31:27 +0200

    Update twrp_gta9wifi.mk

--- twrp_gta9wifi.mk
+++ twrp_gta9wifi.mk
@@ -3,10 +3,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 
 # Inherit from gta9wifi device
 $(call inherit-product, device/samsung/gta9wifi/device.mk)
@@ -17,9 +17,9 @@ PRODUCT_BRAND := samsung
 PRODUCT_MODEL := Galaxy Tab A9
 PRODUCT_MANUFACTURER := samsung
 
-PRODUCT_GMS_CLIENTID_BASE := android-generic
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="gta9wifixx-user 12 SP1A.210812.016 X110XXS6CYI3 release-keys"
 
-BUILD_FINGERPRINT := 
+BUILD_FINGERPRINT := samsung/gta9wifixx/gta9wifi:12/SP1A.210812.016/X110XXS6CYI3:user/release-keys
