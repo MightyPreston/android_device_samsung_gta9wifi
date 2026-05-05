85547ab8c5cfad8c8c3954302ee2209fe2e6e89d
MightyPreston <StudioTube@knueck.ch>
2026-05-05 07:41:42 +0200

    Update twrp_gta9wifi.mk

--- twrp_gta9wifi.mk
+++ twrp_gta9wifi.mk
@@ -7,8 +7,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from gta9wifi device
 $(call inherit-product, device/samsung/gta9wifi/device.mk)
@@ -25,4 +23,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="gta9wifixx-user 12 SP1A.210812.016 X110XXS6CYI3 release-keys"
 
 BUILD_FINGERPRINT := 
-
