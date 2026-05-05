a32ff7e39ac17a94e57664d00eb4db7e37b92f1b
MightyPreston <StudioTube@knueck.ch>
2026-05-05 12:28:02 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -7,3 +7,6 @@ LOCAL_PATH := device/samsung/gta9wifi
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
+
+# Include recovery UI
+TARGET_RECOVERY_UI_LIB := librecovery_ui_default
