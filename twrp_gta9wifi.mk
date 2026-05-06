fbeb08e48c5d9f5255a709fabc9da4e35f051347
MightyPreston <StudioTube@knueck.ch>
2026-05-06 16:05:56 +0200

    Update twrp_gta9wifi.mk

--- twrp_gta9wifi.mk
+++ twrp_gta9wifi.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/samsung/gta9wifi/device.mk)
 
 PRODUCT_DEVICE := gta9wifi
-PRODUCT_NAME := omni_gta9wifi
+PRODUCT_NAME := twrp_gta9wifi
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-X110
 PRODUCT_MANUFACTURER := samsung
