069441a455ed9b392cecb492ee003b8d1c174b23
RootdroidDev <fabian@knueck.ch>
2026-05-06 15:50:06 +0200

    Deleted the fucked up files

--- vendorsetup.sh
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-add_lunch_combo twrp_gta9wifi-user
-add_lunch_combo twrp_gta9wifi-userdebug
-add_lunch_combo twrp_gta9wifi-eng
